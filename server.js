ca700293388806941781406f9ef828fd70733b51
Giye <onengiyeamadi@gmail.com>
2021-08-22 14:15:04 +0100

    Server.js update using express.static() and path.join()

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path= require('path');
 
 
 
 const app = express();
 const port = process.env.PORT || 3030;
 
+app.use('/css', express.static(path.join(__dirname, '/css')));
+app.use('/img', express.static(path.join(__dirname, '/img')));
+
 app.get('/',(req,res)=>{
-    res.sendFile('/index.html');
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 app.listen(port,()=>{
     console.log('CV is online')
